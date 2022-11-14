cc405b30d4d191450669e036be1caca2bf8c7b1f
Juan Jimeno <jimenojmm@gmail.com>
2022-11-15 00:02:15 +0800

    experimenting on right values

--- sphero_rvr/sphero.py
+++ sphero_rvr/sphero.py
@@ -221,7 +221,7 @@ class Sphero(Node):
 
             if 'Velocity' in data:
                 received.add('velocity')
-                self.linear.x = data['Velocity']['X'] * 5.0 / 2 **31
+                self.linear.x = data['Velocity']['Y'] 
 
             if 'Accelerometer' in data:
                 received.add('accelerometer')
